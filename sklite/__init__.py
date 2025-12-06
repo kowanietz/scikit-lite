3849728ab724d6e4e48237529870fe833e07c181
ben <b.kowanietz@outlook.com>
2025-12-06 13:28:17 +0100

    Initial commit

---
+++ sklite/__init__.py
@@ -0,0 +1,15 @@
+from sklite.linear_model import LinearRegression
+from sklite.metrics import (
+    mean_absolute_error,
+    mean_squared_error,
+    r2_score,
+    root_mean_squared_error,
+)
+
+__all__ = [
+    "mean_squared_error",
+    "r2_score",
+    "mean_absolute_error",
+    "root_mean_squared_error",
+    "LinearRegression",
+]
---
+++ sklite/linear_model/__init__.py
@@ -0,0 +1,3 @@
+from sklite.linear_model.linear_regression import LinearRegression
+
+__all__ = ["LinearRegression"]
